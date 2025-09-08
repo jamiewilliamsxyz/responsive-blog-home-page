38bc63930181ddafc7e9893180015764b27a1494
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-09-08 18:25:42 +0100

    Add blog post data

---
+++ data.js
@@ -0,0 +1,44 @@
+export const blogPostsData = [
+  {
+    thumbnailImage: "./images/blog-img-1.png",
+    datePosted: "JULY 23, 2025",
+    title: "Blog One",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
+  },
+  {
+    thumbnailImage: "./images/blog-img-2.png",
+    datePosted: "AUGUST 1, 2025",
+    title: "Blog Two",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
+  },
+  {
+    thumbnailImage: "./images/blog-img-3.png",
+    datePosted: "AUGUST 19, 2025",
+    title: "Blog Three",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
+  },
+  {
+    thumbnailImage: "./images/blog-img-4.png",
+    datePosted: "AUGUST 29, 2025",
+    title: "Blog Four",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
+  },
+  {
+    thumbnailImage: "./images/blog-img-5.png",
+    datePosted: "SEPTEMBER 14, 2025",
+    title: "Blog Five",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
+  },
+  {
+    thumbnailImage: "./images/blog-img-6.png",
+    datePosted: "SEPTEMBER 20, 2025",
+    title: "Blog Six",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
+  },
+];
