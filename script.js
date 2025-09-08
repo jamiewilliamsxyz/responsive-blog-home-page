ea2c61fb90eea46aba8e77a6764ef351fb204dd9
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-09-08 18:36:44 +0100

    Remove current date, add year to copyright on footer and render blog posts

---
+++ script.js
@@ -0,0 +1,20 @@
+import { blogPostsData } from "./data.js";
+
+// Add current year in footer
+
+document.getElementById("copyright-year-el").textContent =
+  new Date().getFullYear();
+
+// Render blog posts
+document.getElementById("blogs-container").innerHTML = blogPostsData
+  .map((blogPost) => {
+    return `
+    <div>
+      <p>${blogPost.datePosted}</p>
+      <img src="${blogPost.thumbnailImage}"/>
+      <h3>${blogPost.title}</h3>
+      <p>${blogPost.description}</p>
+    </div>
+  `;
+  })
+  .join("");
